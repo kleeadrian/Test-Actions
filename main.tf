52b66ad0dad6b04881e59d4de6c67a77d4a5342e
Adrian Lee <kleeadrian@github.com>
2019-09-13 15:03:39 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,18 +13,25 @@ terraform {
 }
 
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "rsg_app_plan" {
   name     = "api-rg-pro"
   location = "Australia East"
 }
 
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_app_service_plan" "app_plan_test" {
   name                = "api-appserviceplan-pro"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.rsg_app_plan.location}"
+  resource_group_name = "${azurerm_resource_group.rsg_app_plan.name}"
 
   sku {
     tier = "Standard"
     size = "S1"
   }
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "appservice-test"
+  location            = "${azurerm_resource_group.rsg_app_plan.location}"
+  resource_group_name = "${azurerm_resource_group.rsg_app_plan.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.app_plan_test.id}"
 }
