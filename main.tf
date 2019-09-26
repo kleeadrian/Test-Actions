465198d6fa7cf4349e9fab554dba25807410d241
Adrian Lee <kleeadrian@github.com>
2019-09-26 12:27:04 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 provider "azurerm" {
-  version = "=1.28.0"
+  version = "=1.33.0"
 }
 
 terraform {
@@ -12,19 +12,27 @@ terraform {
   }
 }
 
-
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "rsg_app_plan" {
   name     = "api-rg-pro"
   location = "Australia East"
 }
 
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_app_service_plan" "app_plan_test" {
   name                = "api-appserviceplan-pro"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-
+  location            = "${azurerm_resource_group.rsg_app_plan.location}"
+  resource_group_name = "${azurerm_resource_group.rsg_app_plan.name}"
+  kind = "Linux"
+  reserved = true
   sku {
     tier = "Standard"
     size = "S1"
   }
 }
+
+resource "azurerm_app_service" "main" {
+  name                = "appservice-test-1231"
+  location            = "${azurerm_resource_group.rsg_app_plan.location}"
+  resource_group_name = "${azurerm_resource_group.rsg_app_plan.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.app_plan_test.id}"
+}
+
